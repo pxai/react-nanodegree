028b788f3b0ca08161ba2c89dfd4b63050645948
pxai <pello_altadill@cuatrovientos.org>
2017-07-04 00:54:55 +0200

    Added lesson3-3 stateless function components

---
+++ 1.fundamentals/contacts-state/src/ListContacts.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+
+// This is a stateless function component!!
+// It does not need this keyword
+// Use whe we just have a render() method
+function ListContacts (props) {
+  return (
+            <ol className="contact-list">
+               { 
+                   props.contacts.map((contact) =>
+                   (
+                       <li key={contact.id} className="contact-list-item">
+                           <div className="contact-avatar" style={{
+                               backgroundImage: `url(${contact.avatarURL})`
+                           }} />
+                           <div className="contact-details">
+                           <p>{contact.name}</p>
+                           <p>{contact.email}</p>
+                               </div>
+                               <button className='contact-remove'>
+                                   Remove
+                                   </button>
+                        </li>
+                   ))
+               }
+                </ol>
+        )
+}
+/*
+class ListContacts extends Component {
+    render () {
+        // console.log('Props debug: ' + this.props);
+        return (
+            <ol className="contact-list">
+               { 
+                   props.contacts.map((contact) =>
+                   (
+                       <li key={contact.id} className="contact-list-item">
+                           <div className="contact-avatar" style={{
+                               backgroundImage: `url(${contact.avatarURL})`
+                           }} />
+                           <div className="contact-details">
+                           <p>{contact.name}</p>
+                           <p>{contact.email}</p>
+                               </div>
+                               <button className='contact-remove'>
+                                   Remove
+                                   </button>
+                        </li>
+                   ))
+               }
+                </ol>
+        )
+    }
+}
+*/
+export default ListContacts;
