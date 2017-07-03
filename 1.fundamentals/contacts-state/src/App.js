94e67d102bb638782699811ddac083bc93ca189b
pxai <pello_altadill@cuatrovientos.org>
2017-07-04 01:02:06 +0200

    Added lesson3-4 contacts are now part of the state

--- 1.fundamentals/contacts-state/src/App.js
+++ 1.fundamentals/contacts-state/src/App.js
@@ -1,30 +1,38 @@
 import React, { Component } from 'react';
 import ListContacts from './ListContacts'
-const contacts = [
-  {
-    "id": "ryan",
-    "name": "Ryan Florence",
-    "email": "ryan@reacttraining.com",
-    "avatarURL": "http://localhost:5001/ryan.jpg"
-  },
-  {
-    "id": "michael",
-    "name": "Michael Jackson",
-    "email": "michael@reacttraining.com",
-    "avatarURL": "http://localhost:5001/michael.jpg"
-  },
-  {
-    "id": "tyler",
-    "name": "Tyler McGinnis",
-    "email": "tyler@reacttraining.com",
-    "avatarURL": "http://localhost:5001/tyler.jpg"
-  }
-]
+/**
+ * React compares the previous output and new output, determines what has changed, and makes these decisions for us. 
+ * This process of determining what has changed in the previous and new outputs is called Reconciliation.
+ */
 class App extends Component {
+  // Move contacts inside so they become part of state
+  state = {
+    contacts : [
+      {
+        "id": "ryan",
+        "name": "Ryan Florence",
+        "email": "ryan@reacttraining.com",
+        "avatarURL": "http://localhost:5001/ryan.jpg"
+      },
+      {
+        "id": "michael",
+        "name": "Michael Jackson",
+        "email": "michael@reacttraining.com",
+        "avatarURL": "http://localhost:5001/michael.jpg"
+      },
+      {
+        "id": "tyler",
+        "name": "Tyler McGinnis",
+        "email": "tyler@reacttraining.com",
+        "avatarURL": "http://localhost:5001/tyler.jpg"
+      }
+    ]
+  }
+
   render() {
     return (
       <div>
-        <ListContacts contacts={contacts} />
+        <ListContacts contacts={this.state.contacts} />
       </div>
     )
   }
